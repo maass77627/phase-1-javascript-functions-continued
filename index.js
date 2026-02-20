3ff4be25c62bc03f088ca1a6ad098fe27a53086e
Kelly Maass <kmaass77627@gmail.com>
2026-02-20 14:20:34 -0600

    done

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 // code your solution here
+// 
+
+
 function saturdayFun(activity="roller-skate") {
-   return `This Saturday, I want to ${activity}!`
+  return `This Saturday, I want to ${activity}!`
+
 }
 
-const mondayWork = function (activity="go to the office") {
-   return `This Monday, I will ${activity}.`
+function mondayWork(activity="go to the office") {
+  return `This Monday, I will ${activity}.`
+
 }
 
 function wrapAdjective(string="*") {
-
-   return function(adjective="special") {
-    return   `You are ${string}${adjective}${string}!`
+   return function (word="special") {
+      return  `You are ${string}${word}${string}!`
    }
 }
 
